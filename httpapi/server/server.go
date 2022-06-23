aecbe3236310a153b2fe7fc5457638c037443761
steeshk <sa30nitk@gmail.com>
2022-06-23 21:00:30 +0530

    Loads app port from config

--- httpapi/server/server.go
+++ httpapi/server/server.go
@@ -1,14 +1,16 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
+	"go-illustration/config"
 	external "go-illustration/httpapi/external/v1"
 	internal "go-illustration/httpapi/internal/v1"
 	"go-illustration/httpapi/server/route"
 )
 
-func StartServer() {
+func StartServer(cfg config.Config) {
 	var routes []route.Route
 	routes = append(routes, external.V1()...)
 	routes = append(routes, internal.V1()...)
@@ -19,6 +21,7 @@ func StartServer() {
 
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
-		return
+		fmt.Printf("Failed to launch server with error: %s\n", err)
+		panic("Failed to start")
 	}
 }
