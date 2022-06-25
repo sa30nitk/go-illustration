337692e46c9ac8f839f6bf4520fef1d3f1ba3a87
steeshk <sa30nitk@gmail.com>
2022-06-25 21:11:19 +0530

    Adds github.com/julienschmidt/httprouter for router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-illustration
 go 1.16
 
 require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/viper v1.12.0
 )
--- httpapi/server/server.go
+++ httpapi/server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"go-illustration/config"
 	external "go-illustration/httpapi/external/v1"
 	internal "go-illustration/httpapi/internal/v1"
@@ -15,12 +16,12 @@ func StartServer(cfg config.Config) error {
 	routes = append(routes, external.V1()...)
 	routes = append(routes, internal.V1()...)
 
+	router := httprouter.New()
 	for _, r := range routes {
-		http.Handle(r.Path, r.HandlerFunc)
+		router.Handler(r.Method, r.Path, r.HandlerFunc)
 	}
+	router.HandlerFunc(http.MethodGet, "/ping", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "pong") })
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "pong") })
-
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.App.Port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.App.Port), router)
 	return err
 }
