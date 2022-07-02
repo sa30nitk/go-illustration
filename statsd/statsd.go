b272ac36171551650cfc187d3b13068c762b53c0
sateesh <sa30nitk@gmail.com>
2022-07-02 11:07:50 +0530

    Removes buffered statsd client

--- statsd/statsd.go
+++ statsd/statsd.go
@@ -1,22 +1,17 @@
 package statsd
 
 import (
-	"time"
-
 	"github.com/quipo/statsd"
 	"go-illustration/config"
 )
 
-func Setup(cfg config.StatsD) (*statsd.StatsdBuffer, error, func()) {
+func Setup(cfg config.StatsD) (*statsd.StatsdClient, error, func()) {
 	client := statsd.NewStatsdClient(cfg.Host, cfg.Prefix)
 	err := client.CreateSocket()
 	if err != nil {
 		return nil, err, func() {}
 	}
-
-	interval := time.Second * 2
-	stats := statsd.NewStatsdBuffer(interval, client)
-	return stats, nil, func() {
-		stats.Close()
+	return client, nil, func() {
+		client.Close()
 	}
 }
